91f0ac2b74e91ab6858c1ab9fb39e807f09b38d1
zhou <1249126625@qq.com>
2020-04-10 13:43:23 +0800

    最近更新

--- StudentManagement/Program.cs
+++ StudentManagement/Program.cs
@@ -18,7 +18,15 @@ namespace StudentManagement
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            //CreateDefaultBuilder 默认的主机生成器 
+            //CreateDefaultBuilder 会加载一些默认配置
+            //如：环境变量(DOTNET开头)、加载命令行参数、加载应用配置、配置的默认日志组件
             WebHost.CreateDefaultBuilder(args)
+           
+            //调用这里面的一些扩展方法，进行自定义的配置
+            //其中一个默认的配置，启用了kestrel
                 .UseStartup<Startup>();
+
+
     }
 }
--- StudentManagement/Startup.cs
+++ StudentManagement/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace StudentManagement
 {
@@ -18,7 +19,7 @@ namespace StudentManagement
         private readonly IConfiguration _configuration;
 
         public Startup(IConfiguration configuration)
-        { 
+        {
             _configuration = configuration;
         }
 
@@ -26,24 +27,75 @@ namespace StudentManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-               
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
-            { 
+            {
                 app.UseDeveloperExceptionPage();
             }
 
+            #region
+
+            //app.Use(async (context, next) =>
+            //{
+            //    //进程名
+            //    //var processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+            //    //var configValue = _configuration["myKey"];
+
+            //    context.Response.ContentType = "text/plain;charset=utf-8";
+
+            //    logger.LogInformation("M1: 传入请求");
+
+            //    await context.Response.WriteAsync("第一个中间件");
+            //    await next();
+
+            //    logger.LogInformation("M1: 传出响应");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+
+            //    logger.LogInformation("M2: 传入请求");
+            //    await next();
+            //    logger.LogInformation("M2: 传出响应");
+            //});
+
+            #endregion
+
+            #region
+
+            ////DefaultFilesOptions options = new DefaultFilesOptions();
+            ////options.DefaultFileNames.Clear();
+            ////options.DefaultFileNames.Add("mydefault.html");
+
+            ////添加默认文件中间件
+            //app.UseDefaultFiles(options);
+
+            ////添加静态文件中间件
+            //app.UseStaticFiles();
+
+            #endregion
+
+            //FileServerOptions options = new FileServerOptions();
+            //options.DefaultFilesOptions.DefaultFileNames.Clear();
+            //options.DefaultFilesOptions.DefaultFileNames.Add("mydefault.html");
+            //app.UseFileServer(options);
+
+            //app.UseFileServer();
+
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+
             app.Run(async (context) =>
             {
-                //进程名
-                //var processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
-
-                var configValue = _configuration["myKey"];
-                await context.Response.WriteAsync(configValue);
+                //throw new Exception("你的请求在管道中发生了一些错误。");
+                //"Hosting ENVIRONMENT:" + env.EnvironmentName
+                await context.Response.WriteAsync("Hello World");
             });
         }
     }
